c02f819262962edf6cee9dad8cae911c1b544a3d
arijit3111w <arijitghosh443322@gmail.com>
2025-10-28 15:40:40 +0530

    deploy ngrok for demo

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { PropertyFeatures, PredictionResponse, DetailedPropertyData } from '@/types/property';
 
-const API_BASE_URL = 'https://c94b0a305ecc.ngrok-free.app';
+const API_BASE_URL = ' https://11802ba34db7.ngrok-free.app';
 
 export const predictPrice = async (features: PropertyFeatures): Promise<PredictionResponse> => {
   const response = await axios.post<PredictionResponse>(
